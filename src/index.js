2cd66f77b1a4a75195f84ae8f7fe91148c18dade
John Ovalle <johnovalle@gmail.com>
2017-12-24 00:56:20 -0500

    get prefabs(?) wired up

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
 import './index.css';
 
 import { RPG, Phaser } from './turnRpg/js/Base';
-import './turnRpg/js/main';
+import './turnRpg/js/prefabs/Prefab';
+import './turnRpg/js/prefabs/TextPrefab';
+import './turnRpg/js/prefabs/TilePrefab';
+import './turnRpg/js/prefabs/Units/Unit';
+import './turnRpg/js/prefabs/Units/EnemyUnit';
+import './turnRpg/js/prefabs/Units/PlayerUnit';
+import './turnRpg/js/prefabs/HUD/Menu';
+import './turnRpg/js/prefabs/HUD/MenuItem';
+import './turnRpg/js/prefabs/HUD/ActionMessage';
+import './turnRpg/js/prefabs/HUD/AttackMenuItem';
+import './turnRpg/js/prefabs/HUD/EnemyMenuItem';
+import './turnRpg/js/prefabs/HUD/PlayerMenuItem';
+import './turnRpg/js/prefabs/HUD/ShowStat';
+
 import './turnRpg/js/states/Boot';
 import './turnRpg/js/states/Loading';
 import './turnRpg/js/states/Battle';
+import './turnRpg/js/main';
+
+console.log(RPG);
