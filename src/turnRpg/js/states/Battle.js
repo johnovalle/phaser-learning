72d5495d6e1fd6e0e50d2738b31bcad3c5a5cdea
John Ovalle <johnovalle@gmail.com>
2017-12-25 11:47:32 -0500

    get hud working but maybe wont continue with this tutorial

--- src/turnRpg/js/states/Battle.js
+++ src/turnRpg/js/states/Battle.js
@@ -43,6 +43,8 @@ RPG.BattleState.prototype.create = function () {
           this.create_prefab(prefab_name, this.level_data.prefabs[prefab_name]);
       }
   }
+
+  this.init_hud();
 };
 
 RPG.BattleState.prototype.create_prefab = function (prefab_name, prefab_data) {
@@ -52,4 +54,49 @@ RPG.BattleState.prototype.create_prefab = function (prefab_name, prefab_data) {
   if (this.prefab_classes.hasOwnProperty(prefab_data.type)) {
       prefab = new this.prefab_classes[prefab_data.type](this, prefab_name, prefab_data.position, prefab_data.properties);
   }
+};
+
+RPG.BattleState.prototype.init_hud = function () {
+  "use strict";
+  var unit_index, player_unit_health;
+
+  // show player actions
+  this.show_player_actions({x: 106, y: 210});
+
+  // show player units
+  this.show_units("player_units", {x: 202, y: 210}, RPG.PlayerMenuItem.prototype.constructor);
+
+  // show enemy units
+  this.show_units("enemy_units", {x: 10, y: 210}, RPG.EnemyMenuItem.prototype.constructor);
+};
+
+RPG.BattleState.prototype.show_units = function (group_name, position, menu_item_constructor) {
+  "use strict";
+  var unit_index, menu_items, unit_menu_item, units_menu;
+
+  // create units menu items
+  unit_index = 0;
+  menu_items = [];
+  this.groups[group_name].forEach(function (unit) {
+      unit_menu_item = new menu_item_constructor(this, unit.name + "_menu_item", {x: position.x, y: position.y + unit_index * 20}, {group: "hud", text: unit.name, style: Object.create(this.TEXT_STYLE)});
+      unit_index += 1;
+      menu_items.push(unit_menu_item);
+  }, this);
+  // create units menu
+  units_menu = new RPG.Menu(this, group_name + "_menu", position, {group: "hud", menu_items: menu_items});
+};
+
+RPG.BattleState.prototype.show_player_actions = function (position) {
+  "use strict";
+  var actions, actions_menu_items, action_index, actions_menu;
+  // available actions
+  actions = [{text: "Attack", item_constructor: RPG.AttackMenuItem.prototype.constructor}];
+  actions_menu_items = [];
+  action_index = 0;
+  // create a menu item for each action
+  actions.forEach(function (action) {
+      actions_menu_items.push(new action.item_constructor(this, action.text + "_menu_item", {x: position.x, y: position.y + action_index * 20}, {group: "hud", text: action.text, style: Object.create(this.TEXT_STYLE)}));
+      action_index += 1;
+  }, this);
+  actions_menu = new RPG.Menu(this, "actions_menu", position, {group: "hud", menu_items: actions_menu_items});
 };
